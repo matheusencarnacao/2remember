9c48d8a0ce85b667432151b3bc5c2bb3246c89e1
Matheus <matheus.encarnacao@hotmail.com>
2019-08-05 06:59:04 -0300

    Adicionado função de base64

---
+++ app/src/main/java/br/com/tworemember/localizer/StringExtension.kt
@@ -0,0 +1,11 @@
+package br.com.tworemember.localizer
+
+import android.util.Base64
+
+fun String.toBase64() : String{
+    return Base64.encodeToString(this.toByteArray(Charsets.UTF_8), Base64.NO_WRAP)
+}
+
+fun String.fromBase64() : String {
+    return String(Base64.decode(this.toByteArray(), Base64.NO_WRAP),  Charsets.UTF_8)
+}
